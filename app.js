14d8495ad0b5d2ada615efd77f82c854468accc2
laizuakram1 <laizuakram98@gmail.com>
2022-10-29 20:01:02 +0600

    create tour schema

--- app.js
+++ app.js
@@ -1,11 +1,37 @@
 const express = require('express')
 const app = express()
 const cors = require("cors");
+const mongoose = require('mongoose');
 
 
 //middleware
 app.use(express.json());
 app.use(cors());
+
+//schema
+const tourSchema = mongoose.Schema({
+  name:{
+    type: String,
+    required: [true, "please write tour name"],
+  },
+  package: {
+    type: String,
+    required: [true, 'provide package name']
+  },
+  location: {
+    type: String,
+    required:[true, 'write tour location']
+  },
+  features: {
+    type: String,
+    require:[true, 'add some features']
+  },
+  price: {
+    type: Number,
+    required:[true, 'add price'],
+    min:[0, "price can't negative"]
+  }
+})
 
 
 app.get('/', (req, res) => {
